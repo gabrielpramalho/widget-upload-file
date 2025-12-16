84a2971bfa2276d147b8982f7c9248fc1a4c4005
Gabriel Ramalho <biel-pedro@hotmail.com>
2025-12-16 10:45:13 -0300

    feat: add circular progress component and add some mocks states

--- src/components/upload-widget-dropzone.tsx
+++ src/components/upload-widget-dropzone.tsx
@@ -1,31 +1,46 @@
 import { useDropzone } from 'react-dropzone'
+import { CircularProgressBar } from './ui/circular-progress-bar'
 
 export function UploadWidgetUploadDropzone() {
+	const isThereAnyPendingUpload = true
+	const uploadGlobalPercentage = 66
+
 	const { getInputProps, getRootProps, isDragActive } = useDropzone({
 		multiple: true,
 		accept: {
 			'image/jpeg': [],
 			'image/png': [],
 		},
-    onDrop(acceptedFiles) {
-      console.log(acceptedFiles)
-    },
+		onDrop(acceptedFiles) {
+			console.log(acceptedFiles)
+		},
 	})
 
 	return (
 		<div className="px-3 flex flex-col gap-3">
-			<div 
-      data-active={isDragActive}
-      className='cursor-pointer text-zinc-400 bg-black/20 p-5 rounded-lg border border-zinc-700 border-dashed h-32 flex flex-col items-center justify-center gap-1 hover:border-zinc-600 transition-colors data-[active=true]:border-lime-500 data-[active=true]:bg-lime-500/10 data-[active=true]:text-lime-400'
-      {...getRootProps()}
-      >
-        <input type='file' {...getInputProps()} />
+			<div
+				data-active={isDragActive}
+				className="cursor-pointer text-zinc-400 bg-black/20 p-5 rounded-lg border border-zinc-700 border-dashed h-32 flex flex-col items-center justify-center gap-1 hover:border-zinc-600 transition-colors data-[active=true]:border-lime-500 data-[active=true]:bg-lime-500/10 data-[active=true]:text-lime-400"
+				{...getRootProps()}
+			>
+				<input type="file" {...getInputProps()} />
 
-        <span className='text-xs'>Drop your files hire or </span>
-        <span className='text-xs underline'>click to open picker</span>
-      </div>
+				{isThereAnyPendingUpload ? (
+					<div className='flex flex-col gap-2.5 items-center'>
+            <CircularProgressBar progress={uploadGlobalPercentage} size={56} strokeWidth={4} />
+            <span className='text-xs'>Uploading 2 files...</span>
+          </div>
+				) : (
+					<>
+						<span className="text-xs">Drop your files hire or </span>
+						<span className="text-xs underline">click to open picker</span>
+					</>
+				)}
+			</div>
 
-      <span className='text-xs text-zinc-400'>Only PNG and JPEG files are supported.</span>
+			<span className="text-xxs text-zinc-400">
+				Only PNG and JPEG files are supported.
+			</span>
 		</div>
 	)
 }
--- src/components/upload-widget-upload-list.tsx
+++ src/components/upload-widget-upload-list.tsx
@@ -1,17 +1,25 @@
-import { UploadWidgetUploadItem } from "./upload-widget-upload-item";
+import { UploadWidgetUploadItem } from './upload-widget-upload-item'
 
 export function UploadWidgetUploadList() {
+	const isUploadListEmpty = false
+
 	return (
 		<div className="px-3 flex flex-col gap-3">
 			<span className="text-xs font-medium">
 				Uploaded files <span>(2)</span>
 			</span>
 
-      <div className="space-y-2">
-        <UploadWidgetUploadItem />
-        <UploadWidgetUploadItem />
-        <UploadWidgetUploadItem />
-      </div>
+			{isUploadListEmpty ? (
+				<div>
+					<span className='text-xs text-zinc-400'>No uploads added</span>
+				</div>
+			) : (
+				<div className="space-y-2">
+					<UploadWidgetUploadItem />
+					<UploadWidgetUploadItem />
+					<UploadWidgetUploadItem />
+				</div>
+			)}
 		</div>
 	)
 }
